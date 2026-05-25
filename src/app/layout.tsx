e51d1ca3031107cf5f8736ea80f97366277a2da6
Designativa <200682893+designativa07@users.noreply.github.com>
2026-05-24 21:06:26 -0300

    chore: adiciona leaflet + react-leaflet + markercluster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import { Providers } from "@/components/layout/providers";
 import { Header } from "@/components/layout/header";
 
